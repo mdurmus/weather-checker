43953feb86603f2f9472ed5353fdd3f9af210a44
mdurmus <mdurmus@outlook.com>
2024-02-16 18:59:04 +0100

    fix: code review for location.py

--- location.py
+++ location.py
@@ -1,17 +1,18 @@
 class Location:
     '''
-    The class object that defines the target 
+    The class object that defines the target
     '''
-    
-    
-    def __init__(self,location_name,latitude,longitude, arrival_date, weather, 
-                 celsius,kelvin,postal_code,country):
-        self.country = country
-        self.location_name = location_name
-        self.latitude = latitude
-        self.longitude = longitude
-        self.arrival_date = arrival_date
-        self.weather = weather
-        self.celsius = celsius
-        self.kelvin = kelvin
-        self.postal_code = postal_code
+
+
+def __init__(self, location_name, latitude, longitude, arrival_date,
+             weather, celsius, kelvin, postal_code, country):
+
+    self.country = country
+    self.location_name = location_name
+    self.latitude = latitude
+    self.longitude = longitude
+    self.arrival_date = arrival_date
+    self.weather = weather
+    self.celsius = celsius
+    self.kelvin = kelvin
+    self.postal_code = postal_code
