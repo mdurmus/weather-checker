ab1070878a739c5791ca61cbe570ddbb10bbd047
mehmet durmus <mdurmus@outlook.com>
2024-05-18 09:08:41 +0000

    ready to deploy

--- tools.py
+++ tools.py
@@ -237,7 +237,6 @@ def send_mail(person):
     for location in person.locations:
         text = f"Location: {location.location_name}\nPostal Code: {location.postal_code}\nCountry: {location.country}\nLatitude: {location.latitude}\nLongitude: {location.longitude}\n Arrival Date: {location.arrival_date}\nWeather: {location.weather}\nCelsius: {location.celsius}°C\nKelvin: {location.kelvin}K\n-----------\n"
     body += text
-    
     password = os.environ.get('MAIL_PASS')
     receiver = person.person_email
     sender = "demo@mehmetdurmus.de"
